1cc84b9042b4f6ec10c39194a657c40ad9a84c6c
MuhAndriJP <m.a.juliansyah@gmail.com>
2021-11-24 22:02:10 +0700

    Menambahkan User Databases dan Controllers

--- controllers/users_controllers.go
+++ controllers/users_controllers.go
@@ -1,1 +1,58 @@
 package controllers
+
+import (
+	"net/http"
+	"project2/lib/databases"
+	"project2/models"
+	"project2/response"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+var user models.Users
+
+func CreateUserControllers(c echo.Context) error {
+	c.Bind(user)
+	user, err := databases.CreateUser(&user)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.BadRequestResponse())
+	}
+	return c.JSON(http.StatusOK, response.SuccessResponseData(user))
+}
+
+func GetUserControllers(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.FalseParamResponse())
+	}
+	user, err := databases.GetUser(id)
+	if err != nil || user == nil {
+		return c.JSON(http.StatusBadRequest, response.BadRequestResponse())
+	}
+	return c.JSON(http.StatusOK, response.SuccessResponseData(user))
+}
+
+func UpdateUserControllers(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.FalseParamResponse())
+	}
+	_, err = databases.UpdateUser(id, &user)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.BadRequestResponse())
+	}
+	return c.JSON(http.StatusOK, response.SuccessResponseNonData())
+}
+
+func DeleteUserControllers(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.FalseParamResponse())
+	}
+	_, err = databases.DeleteUser(id)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.BadRequestResponse())
+	}
+	return c.JSON(http.StatusOK, response.SuccessResponseNonData())
+}
