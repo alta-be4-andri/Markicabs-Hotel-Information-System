2e187c4093b65abee061ae9c98e6348725afb7de
alfynf <alfynurf36@gmail.com>
2021-11-25 12:35:22 +0700

    add token to user model

--- lib/databases/users_databases.go
+++ lib/databases/users_databases.go
@@ -63,6 +63,7 @@ func LoginUsers(user models.UserLogin) (string, error) {
 	if !check {
 		return "", nil
 	}
+	userpassword.Password = hashpassword
 
 	userpassword.Token, err = middlewares.CreateToken(int(userpassword.ID))
 	if err != nil {
