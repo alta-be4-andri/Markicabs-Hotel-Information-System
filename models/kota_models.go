88d8cf9b880bb6142ca27822bc799cf29ebcd7e2
DylanRipper <naufalmehmed@gmail.com>
2021-11-26 11:35:27 +0700

    update city&province

--- models/kota_models.go
+++ models/kota_models.go
@@ -1,9 +1,7 @@
 package models
 
-import "gorm.io/gorm"
-
 type Kota struct {
-	gorm.Model
-	NamaKota   string `gorm:"type:varchar(50);not null" json:"nama_provinsi" form:"nama_provinsi"`
+	Id         int
+	Nama_Kota  string `gorm:"type:varchar(50);not null" json:"nama_provinsi" form:"nama_provinsi"`
 	ProvinsiID uint   `json:"id_provinsi" form:"id_provinsi"`
 }
