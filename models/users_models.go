6d2b8ff6a969f3feb2bcab56588bba39484f9393
DylanRipper <naufalmehmed@gmail.com>
2021-11-30 11:17:55 +0700

    update struct

--- models/users_models.go
+++ models/users_models.go
@@ -6,7 +6,7 @@ type Users struct {
 	gorm.Model
 	Nama         string        `gorm:"type:varchar(255)" json:"nama" form:"nama"`
 	Email        string        `gorm:"type:varchar(100);unique;not null" json:"email" form:"email"`
-	Password     string        `gorm:"type:varchar(100);not null" json:"password" form:"password"`
+	Password     string        `gorm:"type:longtext;not null" json:"password" form:"password"`
 	Phone_Number string        `gorm:"type:varchar(100);unique;not null" json:"phone" form:"phone"`
 	Token        string        `gorm:"type:longtext" json:"token" form:"token"`
 	HomeStay     []HomeStay    `gorm:"foreignKey:UsersID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
