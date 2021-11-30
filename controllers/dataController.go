a76301cb214fd1e3cdfe7f2047acb0129706ecc6
DylanRipper <naufalmehmed@gmail.com>
2021-11-30 16:52:59 +0700

    input data fasilitas

--- controllers/dataController.go
+++ controllers/dataController.go
@@ -8,7 +8,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func inputDataFasilitasCon(c echo.Context) error {
+func InputDataFasilitasCon(c echo.Context) error {
 	input := databases.InputData()
 	if input != nil {
 		return c.JSON(http.StatusBadRequest, response.BadRequestResponse())
