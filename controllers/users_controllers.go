c263acf4f06e31c7631389f2f00be1b87de4bb4f
DylanRipper <naufalmehmed@gmail.com>
2021-11-30 15:43:32 +0700

    tes users

--- controllers/users_controllers.go
+++ controllers/users_controllers.go
@@ -14,6 +14,7 @@ import (
 var user models.Users
 
 func CreateUserControllers(c echo.Context) error {
+	user := models.Users{}
 	c.Bind(&user)
 	newPass, _ := plugins.Encrypt(user.Password)
 	user.Password = newPass
